78db6dc30b4d5bb090e6543baf754ecd73db883c
haixiangyan <haixiang6123@gmail.com>
2021-12-29 23:44:32 +0800

    feat: 初始化 hello world

---
+++ main.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // 处理 Mac OS 点击关闭的问题
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
